294b975b45075762e1287b5d0990536b69f289c7
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-01-27 13:05:48 -0300

    Iniciar repositorio local.

---
+++ components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// styles
+import styles from '../styles/Footer.module.css';
+
+
+
+const Footer = () => (
+		<footer className={styles.footer}>
+			<p>
+				Marcus Vinícius Richa - Web Developer
+			</p>
+		</footer>
+);
+
+
+export default Footer;
+
+
