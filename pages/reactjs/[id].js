5a69ae730333222ff53f8c5a00192e45ed08d875
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-01-29 19:14:35 -0300

    Corrigir chamada para artigo individual na rota Reactjs.

--- pages/reactjs/[id].js
+++ pages/reactjs/[id].js
@@ -1,19 +1,38 @@
 // https://nextjs.org/learn/basics/data-fetching/implement-getstaticprops
 // /reactjs/high-order-components-29-01-2022
-import React from 'react';
 import Head from 'next/head';
 
 // styles
 import styles from '../../styles/Reactjs.module.css';
 
 // articles data
-import { getAllArticlesIds, getSortedArticlesData, getArticleData } from '../../lib/articles'
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
 
+
+
+export default function Post({ postData }) {
+	const { title, id, date } = postData;
+	
+	
+	return (
+		<>
+			<ul>
+				<li>
+					{title}
+					<br />
+					{id}
+					<br />
+					{date}
+				</li>
+			</ul>
+		</>
+	);
+};
 
 
 export async function getStaticPaths() {
-	const paths = getAllArticlesIds();
+	const paths = getAllPostIds();
 	
 	console.log(paths);
 	
@@ -26,61 +45,17 @@ export async function getStaticPaths() {
 
 
 export async function getStaticProps({ params }) {
-	// All articles
-	//const allArticlesData = getSortedArticlesData();
+	const postData = await getPostData(params.id);
 	
-	// Speciffic article
-	const articleData = getArticleData(params.id);
 	
 	return {
 		props: {
-			articleData
+			postData
 		}
 	};
 };
 
 
-export default function ReactjsArticles({ articleData }) {
-	const { title, id, date } = articleData;
 
-	return(
-		<div className={styles.main}>
-      <Head>
-				<title>JS Tech Blog</title>
-				<meta name="description" content="A blog featuring news and technical information about the JavaScript stack and Web related material." /> 
-				
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			
-			<h1 className={styles.title}>
-				REACT-JS ARTICLES
-			</h1>
-			
-			<section>
-			{/*<ul>
-					{allArticlesData.map(({ id, date, title }) => (
-						<li>
-							{title}
-							<br />
-							{id}
-							<br />
-							{date}
-						</li>
-					))}
-			</ul>*/}
-			<ul>
-				<li>
-					{title}
-					<br />
-					{id}
-					<br />
-					{date}
-				</li>
-				</ul>
-			</section>
-			
-		</div>
-	);
-};
 
 
