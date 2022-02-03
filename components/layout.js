7ff5d0c41205ed3225e1d4201d36308caaccc993
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-02-03 20:40:04 -0300

    Alterar modulo MyApp

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,6 @@
+
+
+// styles
 import styles from '../styles/Layout.module.css';
 
 import { Toolbar } from './Toolbar';
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 // styles
 import '../styles/globals.css';
 
@@ -10,6 +12,22 @@ import Layout from '../components/layout';
 function MyApp({ Component, pageProps }) {
   return (
 		<Layout>
+			<Script
+						strategy='lazyOnload'
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+			
+			<Script id="google-analytics" strategy='lazyOnload'>
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){ dataLayer.push(arguments); }
+					
+					gtag('js', new Date());
+
+					gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+				`}
+			</Script>
+			
 			<Component {...pageProps} />
 		</Layout>
 	);
