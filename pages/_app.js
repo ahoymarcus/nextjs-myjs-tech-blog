2f2319c6f267c7efa0dd0b87f47f6bcc699436d0
Marcus Vinicius da Silveira Richa <marcus.richa@sempreceub.com>
2022-02-03 21:20:29 -0300

    Corrigir modulo MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -12,21 +12,22 @@ import Layout from '../components/layout';
 function MyApp({ Component, pageProps }) {
   return (
 		<Layout>
-			<Script
-						strategy='lazyOnload'
-				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-			/>
-			
-			<Script id="google-analytics" strategy='lazyOnload'>
-				{`
-					window.dataLayer = window.dataLayer || [];
-					function gtag(){ dataLayer.push(arguments); }
-					
-					gtag('js', new Date());
+			{/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
-					gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
-				`}
-			</Script>
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
 			
 			<Component {...pageProps} />
 		</Layout>
