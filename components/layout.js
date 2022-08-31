3de6a96e44773b3ea22cb1706a25d533ba50ba36
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-08-31 13:49:42 -0300

    Fix Layout.js typos

--- components/layout.js
+++ components/layout.js
@@ -59,17 +59,17 @@ export default function Layout({ children }) {
 				)}
 			</main>
 			
-			<aside className={style.asideSection}>
-				<div className={style.primeSection}>
-					<div className={style.primeSection__info}>
-						<h1 className={style.primeSection__title}>Land of Dev-Ops</h1>
-						<p className={style.primeSection__desc}>I am a Web Developer with more than 5 years of experience, and currently I am doing master at Software Engineering. My focus is the JS stack with Webpack, React.js, Next.js, besides I also can work with Node.js to construct APIs REST with token authentication. I have experience as Web Designer with responsive pages, mobile first pattern and CSS-patterns. I have a master at Databases and Business Intelligence.</p>
-						<button className={style.primeSectionButton, style.button}><a  className={style.link} target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/marcus-vinicius-richa-183104199/">LinkedIn</a></button>
+			<aside className={styles.asideSection}>
+				<div className={styles.primeSection}>
+					<div className={styles.primeSection__info}>
+						<h1 className={styles.primeSection__title}>Land of Dev-Ops</h1>
+						<p className={styles.primeSection__desc}>I am a Web Developer with more than 5 years of experience, and currently I am doing master at Software Engineering. My focus is the JS stack with Webpack, React.js, Next.js, besides I also can work with Node.js to construct APIs REST with token authentication. I have experience as Web Designer with responsive pages, mobile first pattern and CSS-patterns. I have a master at Databases and Business Intelligence.</p>
+						<button className={styles.primeSectionButton, styles.button}><a  className={styles.link} target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/marcus-vinicius-richa-183104199/">LinkedIn</a></button>
 					</div>
-					<a className={style.link} target="_blank" rel="noreferrer" href="https://my-javascript-bookshelf.netlify.app/">
+					<a className={styles.link} target="_blank" rel="noreferrer" href="https://my-javascript-bookshelf.netlify.app/">
 		
 						<Image 
-							className={style.primeSection__image}
+							className={styles.primeSection__image}
 							src={webPortolioImg1} 
 							alt="online portfolio page icon" 
 							width={200}
@@ -78,11 +78,11 @@ export default function Layout({ children }) {
 					</a>
 				</div>
 				
-				<div className={style.informationSection}>
-					<h2 className={style.informationSectionTitle}>Some of my other works as a JavaScript Developer</h2>
-					<div className={style.informationSection__itemsContainer}>
-						<div className={style.informationSection__item}>
-							<a className={style.link} target="_blank" rel="noreferrer" href="https://redux-reactjs-personal-portfolio-webpage-version-2.netlify.app/" >
+				<div className={styles.informationSection}>
+					<h2 className={styles.informationSectionTitle}>Some of my other works as a JavaScript Developer</h2>
+					<div className={styles.informationSection__itemsContainer}>
+						<div className={styles.informationSection__item}>
+							<a className={styles.link} target="_blank" rel="noreferrer" href="https://redux-reactjs-personal-portfolio-webpage-version-2.netlify.app/" >
 							
 								<Image 
 									src={webPortolioImg2} 
@@ -91,10 +91,10 @@ export default function Layout({ children }) {
 									height={200}
 								/>
 							</a>
-							<p className={style.informationSection__text}>My Online Portfolio, where I present my personal work, and also some of the main studying projects in software development. It features, interaction, and more descriptive meta-data from the projects and from the techs..</p>
+							<p className={styles.informationSection__text}>My Online Portfolio, where I present my personal work, and also some of the main studying projects in software development. It features, interaction, and more descriptive meta-data from the projects and from the techs..</p>
 						</div>
-						<div className={style.informationSection__item}>
-							<a className={style.link} target="_blank" rel="noreferrer" href="https://reactjs-rocket-launches-app.netlify.app/" >
+						<div className={styles.informationSection__item}>
+							<a className={styles.link} target="_blank" rel="noreferrer" href="https://reactjs-rocket-launches-app.netlify.app/" >
 							
 							<Image 
 								src={rocketImg1} 
@@ -103,10 +103,10 @@ export default function Layout({ children }) {
 								height={200}
 							/>
 							</a>
-							<p className={style.informationSection__text}>This is a React-app written to be a client of the Rocket Launcher API that makes its authentication through JWT tokens and rendering the information about launches and rockets received by the endpoints from the backend.</p>
+							<p className={styles.informationSection__text}>This is a React-app written to be a client of the Rocket Launcher API that makes its authentication through JWT tokens and rendering the information about launches and rockets received by the endpoints from the backend.</p>
 						</div>
-						<div className={style.informationSection__item}>
-							<a className={style.link} target="_blank" rel="noreferrer" href="https://github.com/ahoymarcus/expressjs-rocket-launches-api" >
+						<div className={styles.informationSection__item}>
+							<a className={styles.link} target="_blank" rel="noreferrer" href="https://github.com/ahoymarcus/expressjs-rocket-launches-api" >
 								<Image 
 									src={rocketImg2} 
 									alt="rocket launche app icon" 
@@ -114,11 +114,11 @@ export default function Layout({ children }) {
 									height={200}
 								/>
 							</a>
-							<p className={style.informationSection__text}>This is the REST API version for the React-app, and it uses Node-js to arrange a small web-service gathering information from a larger public API about the SpaceX project. It also authenticatis with JWT.</p>
+							<p className={styles.informationSection__text}>This is the REST API version for the React-app, and it uses Node-js to arrange a small web-service gathering information from a larger public API about the SpaceX project. It also authenticatis with JWT.</p>
 						</div>
 						
-						<div className={style.informationSection__item}>
-							<a className={style.link} target="_blank" rel="noreferrer" href="https://my-javascript-bookshelf.netlify.app/" >
+						<div className={styles.informationSection__item}>
+							<a className={styles.link} target="_blank" rel="noreferrer" href="https://my-javascript-bookshelf.netlify.app/" >
 							
 								<Image 
 									src={myJSBookshelfImg} 
@@ -127,10 +127,10 @@ export default function Layout({ children }) {
 									height={200}
 								/>
 							</a>
-							<p className={style.informationSection__text}>A personal project for a book library entirely made with JavaScript. Modeled with UML and designed with Figma. It uses Webpack’s framework to create the build for the app and manage its web dependencies. It uses MVC architectural pattern.</p>
+							<p className={styles.informationSection__text}>A personal project for a book library entirely made with JavaScript. Modeled with UML and designed with Figma. It uses Webpack’s framework to create the build for the app and manage its web dependencies. It uses MVC architectural pattern.</p>
 						</div>
-						<div className={style.informationSection__item}>
-							<a className={style.link} target="_blank" rel="noreferrer" href="https://musica-erudita.netlify.app/" >
+						<div className={styles.informationSection__item}>
+							<a className={styles.link} target="_blank" rel="noreferrer" href="https://musica-erudita.netlify.app/" >
 							
 								<Image 
 									src={musEruditaImg} 
@@ -139,34 +139,34 @@ export default function Layout({ children }) {
 									height={200}
 								/>
 							</a>
-							<p className={style.informationSection__text}>My personal site about Music, made early in 2018 to practice my skills in Web Design at that time. They are a set of web pages, all responsive and following the mobile-first pattern. They also use SASS and have a SVG animation of my own creation.</p>
+							<p className={styles.informationSection__text}>My personal site about Music, made early in 2018 to practice my skills in Web Design at that time. They are a set of web pages, all responsive and following the mobile-first pattern. They also use SASS and have a SVG animation of my own creation.</p>
 						</div>
 							
 					</div>
 				</div>
 				
-				<div className={style.testimonialSection}>
-					<p className={style.testimonialSection__text}>Node.js is a cross-platform environment and library for running JavaScript applications which is used to create networking and server-side applications. The definition given by its official documentation is as follows: </p>
-					<p className={style.testimonialSection__text}>It was built on Chrome&aposs JavaScript runtime for easily building fast and scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.</p>
-					<p className={style.testimonialSection__text}>It also provides a rich library of various JavaScript modules to simplify the development of web applications and has features like:</p>
-					<p className={style.testimonialSection__text}>I ---  Extremely fast: Node.js is built on Google Chrome&aposs V8 JavaScript Engine, so its library is very fast in code execution.</p>
-					<p className={style.testimonialSection__text}>II --- I/O is Asynchronous and Event Driven: All APIs of Node.js library are asynchronous i.e. non-blocking. So a Node.js based server never waits for an API to return data. The server moves to the next API after calling it and a notification mechanism of Events of Node.js helps the server to get a response from the previous API call. It is also a reason that it is very fast.</p>
-					<p className={style.testimonialSection__text}>III --- Single threaded: Node.js follows a single threaded model with event looping.</p>
-					<p className={style.testimonialSection__text}>IV ---  Highly Scalable: Node.js is highly scalable because event mechanism helps the server to respond in a non-blocking way.</p>
-					<p className={style.testimonialSection__text}>V --- No buffering: Node.js cuts down the overall processing time while uploading audio and video files. Node.js applications never buffer any data. These applications simply output the data in chunks.</p>
-					<p className={style.testimonialSection__text}>VI --- Open source: Node.js has an open source community which has produced many excellent modules to add additional capabilities to Node.js applications.</p>
-					<p className={style.testimonialSection__text}>VII --- License: Node.js is released under the MIT license.</p>
+				<div className={styles.testimonialSection}>
+					<p className={styles.testimonialSection__text}>Node.js is a cross-platform environment and library for running JavaScript applications which is used to create networking and server-side applications. The definition given by its official documentation is as follows: </p>
+					<p className={styles.testimonialSection__text}>It was built on Chrome&aposs JavaScript runtime for easily building fast and scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.</p>
+					<p className={styles.testimonialSection__text}>It also provides a rich library of various JavaScript modules to simplify the development of web applications and has features like:</p>
+					<p className={styles.testimonialSection__text}>I ---  Extremely fast: Node.js is built on Google Chrome&aposs V8 JavaScript Engine, so its library is very fast in code execution.</p>
+					<p className={styles.testimonialSection__text}>II --- I/O is Asynchronous and Event Driven: All APIs of Node.js library are asynchronous i.e. non-blocking. So a Node.js based server never waits for an API to return data. The server moves to the next API after calling it and a notification mechanism of Events of Node.js helps the server to get a response from the previous API call. It is also a reason that it is very fast.</p>
+					<p className={styles.testimonialSection__text}>III --- Single threaded: Node.js follows a single threaded model with event looping.</p>
+					<p className={styles.testimonialSection__text}>IV ---  Highly Scalable: Node.js is highly scalable because event mechanism helps the server to respond in a non-blocking way.</p>
+					<p className={styles.testimonialSection__text}>V --- No buffering: Node.js cuts down the overall processing time while uploading audio and video files. Node.js applications never buffer any data. These applications simply output the data in chunks.</p>
+					<p className={styles.testimonialSection__text}>VI --- Open source: Node.js has an open source community which has produced many excellent modules to add additional capabilities to Node.js applications.</p>
+					<p className={styles.testimonialSection__text}>VII --- License: Node.js is released under the MIT license.</p>
 
-					<span className={style.testimonialSection__author}><a className={style.link} target="_blank" rel="noreferrer" href="https://www.javatpoint.com/nodejs-tutorial">JavaTPoint Tutorials</a></span>
+					<span className={styles.testimonialSection__author}><a className={styles.link} target="_blank" rel="noreferrer" href="https://www.javatpoint.com/nodejs-tutorial">JavaTPoint Tutorials</a></span>
 				</div>
 				
-				<div className={style.actionSection}>
-					<div className={style.actionSection__callToAction}>
+				<div className={styles.actionSection}>
+					<div className={styles.actionSection__callToAction}>
 						<div>
-							<h3 className={style.actionSection__title}>GitHub</h3>
-							<p className={style.actionSection__text}>Come and visit me at my GitHub repository!</p>
+							<h3 className={styles.actionSection__title}>GitHub</h3>
+							<p className={styles.actionSection__text}>Come and visit me at my GitHub repository!</p>
 						</div>
-						<button className={style.actionSection__button, style.button}><a className={style.link} target="_blank" rel="noreferrer" href="https://github.com/ahoymarcus">Enter</a></button>
+						<button className={styles.actionSection__button, styles.button}><a className={styles.link} target="_blank" rel="noreferrer" href="https://github.com/ahoymarcus">Enter</a></button>
 					</div>
 				</div>
 			</aside>
